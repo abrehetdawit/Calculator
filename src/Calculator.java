e25cf570560988678d6fe3cca7cfc57d7a8b4787
Abrehet <Gualwediasfaha@gmail.com>
2019-01-19 12:55:17 -0500

    the third one

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
         return num1+num2;
     }
     public static double subtract(double num1, double num2){
-        return num1-num2;
+        double result =num1-num2;
+        return result;
     }
 }
