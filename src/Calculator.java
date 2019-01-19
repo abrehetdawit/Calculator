a117c413fd5ddbe6178c96f450478dcd39a565e9
abrehetdawit <46763559+abrehetdawit@users.noreply.github.com>
2019-01-19 17:00:30 -0500

    add int method

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,5 +14,9 @@ public class Calculator {
     } 
     public static double division(double num1,double num2){
          return num1/num2;
+    }  
+    
+    public static int division(int num1,int num2){
+         return num1/num2;
     }    
 }
